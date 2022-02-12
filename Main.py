ec3bdcfc746feb4916a292935a04e78dbdd95ff4
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-02-12 17:10:37 -0500

    k

---
+++ Main.py
@@ -0,0 +1,33 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+
+import torch
+import torchvision
+from torchvision import datasets, models
+from torchvision.transforms import functional as FT
+from torchvision import transforms as T
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, sampler, random_split, Dataset
+import copy
+import math
+from PIL import Image
+import cv2
+import albumentations as A  # our data augmentation library
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+# remove arnings (optional)
+import warnings
+warnings.filterwarnings("ignore")
+from collections import defaultdict, deque
+import datetime
+import time
+from tqdm import tqdm # progress bar
+from torchvision.utils import draw_bounding_boxes
+
+from pycocotools.coco import COCO
+
