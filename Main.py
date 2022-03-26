48d5e48243bcd71792679511af006740330228d4
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-03-26 05:37:36 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ USE_CHECKPOINT = True
 IMAGE_SIZE = 2336 # Row and column number 2180
 DATASET_PATH = "./HBCOSA_1/"
 NUMBER_EPOCH = 500
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.001
 BATCH_SIZE = int(32*2) # Initially just 4
 
 # Transformation Parameters:
