a1548011938d02c05d3e80827e46c3756436b78e
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-03-31 09:27:01 -0400

    Update To_Predict.py

--- To_Predict.py
+++ To_Predict.py
@@ -30,7 +30,7 @@ PREDICTED_PATH = "./Images/Prediction_Images/Predicted_Images/"
 SAVE_FULL_IMAGES = True
 SAVE_CROPPED_IMAGES = False
 DIE_SPACING_SCALE = 0.99
-MIN_SCORE = 0.60
+MIN_SCORE = 0.70
 
 
 
