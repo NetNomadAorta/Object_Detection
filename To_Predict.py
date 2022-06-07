3bdb16f7023544684de4d34ab03d3b91e4e5548e
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-06-07 19:05:13 -0400

    Update To_Predict.py

--- To_Predict.py
+++ To_Predict.py
@@ -19,17 +19,19 @@ import shutil
 
 
 # User parameters
-SAVE_NAME_OD = "./Models-OD/Window_Tightness-0.model"
+SAVE_NAME_OD = "./Models-OD/SMiPE4-1090.model"
 DATASET_PATH = "./Training_Data/" + SAVE_NAME_OD.split("./Models-OD/",1)[1].split("-",1)[0] +"/"
 IMAGE_SIZE              = int(re.findall(r'\d+', SAVE_NAME_OD)[-1] ) # Row and column number 
-TO_PREDICT_PATH         = "./Images/Prediction_Images/To_Predict/"
-PREDICTED_PATH          = "./Images/Prediction_Images/Predicted_Images/"
+# TO_PREDICT_PATH         = "./Images/Prediction_Images/To_Predict/"
+TO_PREDICT_PATH         = "//mcrtp-sftp-01/aoitool/SMiPE4-597/0081/"            # USE FOR XDisplay LOTS!
+# PREDICTED_PATH          = "./Images/Prediction_Images/Predicted_Images/"
+PREDICTED_PATH          = "//mcrtp-sftp-01/aoitool/SMiPE4-597-Cropped/0081/"    # USE FOR XDisplay LOTS!
 # PREDICTED_PATH        = "C:/Users/troya/.spyder-py3/ML-Defect_Detection/Images/Prediction_Images/To_Predict_Images/"
-SAVE_ANNOTATED_IMAGES   = True
+SAVE_ANNOTATED_IMAGES   = False
 SAVE_ORIGINAL_IMAGE     = False
-SAVE_CROPPED_IMAGES     = False
+SAVE_CROPPED_IMAGES     = True
 DIE_SPACING_SCALE       = 0.99
-MIN_SCORE               = 0.6
+MIN_SCORE               = 0.4
 
 
 def time_convert(sec):
@@ -145,8 +147,8 @@ model_1.eval()
 torch.cuda.empty_cache()
 
 transforms_1 = A.Compose([
-    # A.Resize(IMAGE_SIZE, IMAGE_SIZE), # our input size can be 600px
-    # A.Rotate(limit=[90,90], always_apply=True),
+    A.Resize(IMAGE_SIZE, IMAGE_SIZE), # our input size can be 600px
+    A.Rotate(limit=[90,90], always_apply=True),
     ToTensorV2()
 ])
 
