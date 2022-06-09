a3c81a7942e25c17304976962a7adfaca0b95f5a
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-06-09 13:32:48 -0400

    k

--- COCO_Json_Maker-Using_OD.py
+++ COCO_Json_Maker-Using_OD.py
@@ -24,7 +24,7 @@ import shutil
 
 
 # User parameters
-SAVE_NAME_OD = "./Models-OD/Window_Tightness-0.model"
+SAVE_NAME_OD = "./Models-OD/E_Electrode-OD-703.model"
 DATASET_PATH = "./Training_Data/" + SAVE_NAME_OD.split("./Models-OD/",1)[1].split("-",1)[0] +"/"
 
 IMAGE_SIZE              = int(re.findall(r'\d+', SAVE_NAME_OD)[-1] ) # Row and column number 
@@ -33,7 +33,7 @@ PREDICTED_PATH          = "./Images/Prediction_Images/Predicted_Images/"
 # PREDICTED_PATH        = "C:/Users/troya/.spyder-py3/ML-Defect_Detection/Images/Prediction_Images/To_Predict_Images/"
 SAVE_ANNOTATED_IMAGES   = True
 SAVE_CROPPED_IMAGES     = False
-MIN_SCORE               = 0.6
+MIN_SCORE               = 0.7
 NUMBER_TO_RUN = 100
 NUMBER_DIE_PER_IMAGE = 0
 
