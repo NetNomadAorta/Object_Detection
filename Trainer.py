6ffbbd235623f9723b8d5aa247b53bd118ca9117
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-06-20 22:10:04 -0400

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -20,11 +20,11 @@ from albumentations.pytorch import ToTensorV2
 
 
 # User parameters
-SAVE_NAME      = "./Models-OD/Window_Edge_Finder-OD-1000.model"
+SAVE_NAME      = "./Models-OD/E_Electrode-OD-703.model"
 USE_CHECKPOINT = True
 IMAGE_SIZE     = int(re.findall(r'\d+', SAVE_NAME)[-1] ) # Row and column number 
 DATASET_PATH   = "./Training_Data/" + SAVE_NAME.split("./Models-OD/",1)[1].split("-",1)[0] +"/"
-NUMBER_EPOCH   = 1000
+NUMBER_EPOCH   = 300
 SCALER         = 1
 # LEARNING_RATE  = 0.0005*SCALER  # Default: Home_PC: 0.01;        Work_PC: 0.0005
 # BATCH_SIZE     = int(16*SCALER) # Default: Home_PC: int(16*4);   Work_PC: int(16*1)
